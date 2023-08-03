c87fb14dd942595818189d06ec8e766d9e28539d
TTM-THUYNT\ttm-dev <huynt@trithucmoi.co>
2023-08-03 10:06:04 +0700

    First commit

---
+++ pages/login/index.tsx
@@ -0,0 +1,97 @@
+import { FC, FormEvent } from "react";
+import { Form, Input, Button, Checkbox, Card, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import Image from "next/image";
+import avatar from "@/images/avatar.png";
+const { Title } = Typography;
+
+interface Values {
+  username: string;
+  password: string;
+  remember: boolean;
+}
+
+const LoginForm: FC = () => {
+  const onFinish = (values: Values) => {
+    console.log("Received values of form: ", values);
+    if (values.remember) {
+      localStorage.setItem("username", values.username);
+      localStorage.setItem("password", values.password);
+    }
+  };
+
+  const handleForgotPassword = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Handle password recovery logic here");
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card hoverable style={{ width: 500 }}>
+        {/* <div>
+          <Image src={avatar} alt="alt" />
+        </div> */}
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Title level={3}>Login Account </Title>
+        </div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              block
+            >
+              Log in
+            </Button>
+            <div style={{ paddingTop: "5px" }}>
+              Don't have an account{" "}
+              <Link href="/register" style={{ color: "#1677ff" }}>
+                sign up
+              </Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ pages/register/index.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { Button, Card, DatePicker, Form, Input, Typography } from "antd";
+import {
+  LockOutlined,
+  MailOutlined,
+  PhoneOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+
+const { Title } = Typography;
+
+
+export default function RegisterForm() {
+  const [form] = Form.useForm();
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card style={{ width: 500 }} hoverable>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Title level={3}>Register Account</Title>
+        </div>
+        <Form name="register_form" className="register-form" form={form}>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your Email!" }]}
+          >
+            <Input
+              prefix={<MailOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item
+            name="confirmPassword"
+            rules={[
+              {
+                required: true,
+                message: "Please input your confirm Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Item>
+          <Form.Item name="phone">
+            <Input
+              prefix={<PhoneOutlined className="site-form-item-icon" />}
+              placeholder="Phone"
+            />
+          </Form.Item>
+          <Form.Item name="dob">
+            <DatePicker style={{ width: "100%" }} />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="register-form-button"
+              block
+            >
+              Register account
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+}
